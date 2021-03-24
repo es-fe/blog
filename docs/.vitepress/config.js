cfeb5e8c1cd25ad0cb9d37434eacd5341a7e61ed
江铓 <jiang.mang@shipout.cn>
2021-03-24 17:30:38 +0800

    deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  title: 'Hello VitePress',
-  description: 'Just playing around.',
-  base: '/blog/'
+  title: 'EzeeShip & Shipout FrontEnd Team Blog',
+  description: 'EzeeShip & Shipout FrontEnd Team',
+  base: '/blog/',
+  themeConfig: {
+    displayAllHeaders: true,
+    sidebar: [
+      { text: '前端DSL实践指南', link: '/前端DSL实践指南' },
+    ]
+  }
 }
